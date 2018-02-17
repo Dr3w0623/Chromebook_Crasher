8993ec4831fd6eddedddd388e234775e78343a64
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2018-02-16 19:25:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var doggo = document.querySelector("#doggo");
 
 var cursorArray = ['url("http://jantimon.nl/running_man/running_man_1.cur"), auto',
-  'url("http://jantimon.nl/running_man/running_man_2.cur"), auto',
-  'url("http://jantimon.nl/running_man/running_man_3.cur"), auto',
-  'url("http://jantimon.nl/running_man/running_man_4.cur"), auto',
-  'url("http://jantimon.nl/running_man/running_man_5.cur", auto'
+  'url("frame_00_delay-0.05s.gif"), auto',
+  'url("frame_01_delay-0.05s.gif"), auto',
+  'url("frame_02_delay-0.05s.gif"), auto',
+  'url("frame_03_delay-0.05s.gif"), auto',
+  'url("frame_04_delay-0.05s.gif"), auto',
+  'url("frame_05_delay-0.05s.gif"), auto',
+  'url("frame_06_delay-0.05s.gif"), auto',
+  'url("frame_07_delay-0.05s.gif"), auto',
+  'url("frame_08_delay-0.05s.gif"), auto',
+  'url("frame_09_delay-0.05s.gif"), auto',
+  'url("frame_10_delay-0.05s.gif"), auto',
+  'url("frame_11_delay-0.05s.gif"), auto',
+  'url("frame_12_delay-0.05s.gif"), auto',
+  'url("frame_13_delay-0.05s.gif"), auto',
+  'url("frame_14_delay-0.05s.gif"), auto',
+  'url("frame_15_delay-0.05s.gif"), auto',
+  'url("frame_16_delay-0.05s.gif"), auto',  
+  'url("frame_17_delay-0.05s.gif"), auto',               
 ];
 i = 0;
 (function cursor() {
